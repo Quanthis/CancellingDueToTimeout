2dd2b31ef75abcd51916ea063fed9661008b6994
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-03-05 12:46:44 +0100

    Dodaj pliki projektów.

---
+++ CancellingDueToTimeout/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CancellingDueToTimeout
+{
+    class Program
+    {
+        static async Task IssueTimeout()
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            cts.CancelAfter(TimeSpan.FromSeconds(5));
+            await Task.Delay(TimeSpan.FromSeconds(10), token);
+
+            WriteLine("I've managed to finish my work");
+        }
+
+        static async Task Main(string[] args)
+        {
+            IssueTimeout();
+            ReadKey();
+        }
+    }
+}
